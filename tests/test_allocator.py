7227c8ca59d42ace22f881a9d5fd28e6038af399
Levan Iordanishvili <levandev9053@gmail.com>
2024-02-28 13:35:27 -0500

    add unit tests

--- tests/test_allocator.py
+++ tests/test_allocator.py
@@ -1,8 +1,52 @@
 import unittest
+from allocator import allocate_resources
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class AllocatorTestCase(unittest.TestCase):
+    def test_allocate_resources_by_different_priorities(self):
+        resources = [
+            {"name": "A", "hours": 1, "priority": 3},
+            {"name": "B", "hours": 1, "priority": 2},
+            {"name": "C", "hours": 1, "priority": 1},
+        ]
+        slots = [
+            ["Slot 1", 1],
+            ["Slot 2", 2],
+            ["Slot 3", 3],
+        ]
+        result = allocate_resources(resources, slots)
+        self.assertEqual(3, len(result))
+        self.assertEqual("C", result[0][0]) # Resource C was prioritized
+        self.assertEqual("B", result[1][0])
+        self.assertEqual("A", result[2][0])
+
+    def test_allocate_resources_by_num_resources_less_than_slots(self):
+        resources = [
+            {"name": "A", "hours": 1, "priority": 3},
+            {"name": "B", "hours": 1, "priority": 2},
+            {"name": "C", "hours": 1, "priority": 1},
+        ]
+        slots = [
+            ["Slot 1", 1],
+            ["Slot 2", 2],
+            ["Slot 3", 3],
+            ["Slot 4", 4],
+        ]
+        result = allocate_resources(resources, slots)
+        self.assertEqual(4, len(result))
+        self.assertEqual("Unallocated", result[3][0])
+
+    def test_allocate_resources_by_num_resources_more_than_slots(self):
+        resources = [
+            {"name": "A", "hours": 1, "priority": 3},
+            {"name": "B", "hours": 1, "priority": 2},
+            {"name": "C", "hours": 1, "priority": 1},
+        ]
+        slots = [
+            ["Slot 1", 1],
+            ["Slot 2", 2],
+        ]
+        with self.assertRaises(ValueError):
+            allocate_resources(resources, slots)
 
 if __name__ == '__main__':
     unittest.main()
