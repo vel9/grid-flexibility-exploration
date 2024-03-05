fbe6bce1c99449f5099e734c55e22ddc6438627a
Levan Iordanishvili <levandev9053@gmail.com>
2024-03-05 18:57:29 -0500

    improve variable names and add pydoc

--- allocator.py
+++ allocator.py
@@ -1,46 +1,60 @@
 from operator import itemgetter
 
 
-def allocate_resources(resources, data_averaged_by_hour_sorted):
-    if len(resources) > len(data_averaged_by_hour_sorted):
+def allocate_resources(resources, time_slots):
+    """
+    Assign resources to slots and create representations of any unassigned slots
+
+    :param resources: list resources to be assigned
+    :param time_slots: list of available time slots
+    :return: allocated list of resources and time slots
+    """
+    if len(resources) > len(time_slots):
         raise ValueError("Can't allocate more resources than slots")
 
-    # sort by priority asc
     resources_sorted = sorted(resources, key=itemgetter("priority"))
     allocated = []
-    i = 0
+    allocated_slot = 0
     for resource in resources_sorted:
         for hour in range(resource["hours"]):
             allocated.append([resource["name"],
-                              data_averaged_by_hour_sorted[i][0],
-                              data_averaged_by_hour_sorted[i][1]])
-            i += 1
+                              time_slots[allocated_slot][0],
+                              time_slots[allocated_slot][1]])
+            allocated_slot += 1
 
-    for rem_i in range(len(data_averaged_by_hour_sorted) - i):
+    for remaining_slot in range(len(time_slots) - allocated_slot):
         allocated.append(["Nothing Scheduled",
-                          data_averaged_by_hour_sorted[i + rem_i][0],
-                          data_averaged_by_hour_sorted[i + rem_i][1]])
+                          time_slots[allocated_slot + remaining_slot][0],
+                          time_slots[allocated_slot + remaining_slot][1]])
 
     return allocated
 
 
-def allocate_resources_parallel(resources, data_averaged_by_hour_sorted):
+def allocate_resources_parallel(resources, time_slots):
+    """
+    Assign resources to time slots, assign as many
+    resources to a single slot as its output value allows
+
+    :param resources: list resources to be assigned
+    :param time_slots: list of available time slots
+    :return: allocated list of resources and time slots
+    """
     # sort by priority asc
     resources_sorted = sorted(resources, key=itemgetter("priority"))
     allocated = []
-    for window in data_averaged_by_hour_sorted:
+    for window in time_slots:
         window_start_time = window[0]
         remaining_output = window[1]
         for resource in resources_sorted:
             if resource["hours"] == 0:
                 continue
-            if (remaining_output - resource["demand"]) >= 0:
+            if (remaining_output - resource["demand_per_hour"]) >= 0:
                 allocated.append([resource["name"],
                                   window_start_time,
-                                  resource["demand"],
+                                  resource["demand_per_hour"],
                                   resource["priority"]])
                 resource["hours"] -= 1
-                remaining_output -= resource["demand"]
+                remaining_output -= resource["demand_per_hour"]
 
         if remaining_output > 0:
             allocated.append(["Nothing Scheduled",
