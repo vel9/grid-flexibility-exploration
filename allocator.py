b403750b53185e595db8666ab14217906317df3f
Levan Iordanishvili <levandev9053@gmail.com>
2024-03-06 13:19:41 -0500

    once resource's hours are 0, remove them from further consideration

--- allocator.py
+++ allocator.py
@@ -46,22 +46,6 @@ def allocate_resources(resources: list[Resource], time_slots):
     return allocated
 
 
-def to_resources(resources_data):
-    """
-    covert each dict to a Resource object and return list
-
-    :param resources_data: dict of resource values
-    :return: list of Resource objects
-    """
-    resources = []
-    for resource in resources_data:
-        resources.append(Resource(resource["name"],
-                                  resource["priority"],
-                                  resource["hours"],
-                                  resource.get("demand_per_hour", float("inf"))))
-    return resources
-
-
 def allocate_resources_parallel(resources: list[Resource], time_slots):
     """
     Assign resources to time slots, assign as many
@@ -77,16 +61,21 @@ def allocate_resources_parallel(resources: list[Resource], time_slots):
     for window in time_slots:
         window_start_time = window[0]
         remaining_output = window[1]
-        for resource in resources_sorted:
-            if resource.hours == 0:
-                continue
+        resources_to_remove = []
+        for index, resource in enumerate(resources_sorted):
             if (remaining_output - resource.demand_per_hour) >= 0:
                 allocated.append([resource.name,
                                   window_start_time,
                                   resource.demand_per_hour,
                                   resource.priority])
                 resource.hours -= 1
+                if resource.hours == 0:
+                    resources_to_remove.append(index)
                 remaining_output -= resource.demand_per_hour
+
+        # once all of resource's hours have been satisfied, remove from list
+        for index_to_remove in reversed(resources_to_remove):
+            resources_sorted.pop(index_to_remove)
 
         if remaining_output > 0:
             allocated.append(["Nothing Scheduled",
@@ -94,3 +83,19 @@ def allocate_resources_parallel(resources: list[Resource], time_slots):
                               remaining_output,
                               float('inf')])
     return allocated
+
+
+def to_resources(resources_data):
+    """
+    covert each dict to a Resource object and return list
+
+    :param resources_data: dict of resource values
+    :return: list of Resource objects
+    """
+    resources = []
+    for resource in resources_data:
+        resources.append(Resource(resource["name"],
+                                  resource["priority"],
+                                  resource["hours"],
+                                  resource.get("demand_per_hour", float("inf"))))
+    return resources
